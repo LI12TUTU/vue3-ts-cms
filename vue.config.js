68a591d92905f543b9d9e8cc99845800203af735
LRY <940049366@qq.com>
2022-03-20 01:55:16 +0800

    feat(vuex vuerouter): add vuex and vuerouter, modify vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
+const path = require("path")
 const { defineConfig } = require("@vue/cli-service")
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // configureWebpack: {
+  //   resolve: {
+  //     alias: {
+  //       compents: "@/components"
+  //     }
+  //   }
+  // }
+  // configureWebpack: (config) => {
+  //   config.resolve.alias = {
+  //     "@": path.resolve(__dirname, "src"),
+  //     components: "@/componts"
+  //   }
+  // }
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set("@", path.resolve(__dirname, "src"))
+      .set("components", "@/components")
+  }
 })
